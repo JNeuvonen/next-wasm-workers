84e981a864cfcf4eca6b9ea5997b72afeab933ef
Jarno Neuvonen <neuvonenjarno@gmail.com>
2022-08-19 14:24:59 +0300

    Commit

--- src/wasm.worker.ts
+++ src/wasm.worker.ts
@@ -1,6 +1,6 @@
 // Needed to declare this as a module. Also shows that imports
 // function normally in workers.
-import { Universe } from '../pkg';
+import { Universe } from '../rust/pkg';
 import { shared } from './shared';
 
 const ctx: Worker = self as unknown as Worker;
@@ -9,7 +9,8 @@ let gamePaused = false;
 let universe: Universe | null = null;
 
 async function startGameOfLife(width: number, height: number) {
-  const { Universe } = await import('../pkg');
+  const { Universe } = await import('../rust/pkg');
+  console.log(universe);
   universe = Universe.new(width, height);
 }
 
@@ -25,16 +26,6 @@ async function tick() {
 
 async function pauseAndPlayGame() {
   gamePaused = !gamePaused;
-}
-
-async function start() {
-  // From https://github.com/wasm-tool/wasm-pack-plugin
-  const { get_rust_data, Universe } = await import('../rust/pkg');
-
-  ctx.postMessage({
-    type: 'rustData',
-    data: get_rust_data(),
-  });
 }
 
 ctx.addEventListener('message', (evt) => {
